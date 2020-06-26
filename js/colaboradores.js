cd89407e7326ed53b668c5708bb464cb839488c8
Valeria <valeria.toscano.95@hotmail.com>
2020-06-26 18:13:57 -0500

    rebe fundadora

--- js/colaboradores.js
+++ js/colaboradores.js
@@ -6,13 +6,6 @@ var colaboradores=[
      img: "../../imgs/Equipo/jc.png",
      prof:"Desarrollador Web",
      description: "Estudiante de Ingeniería Mecatrónica con experiencia en desarrollo web e interés por iniciativas sociales."},
-    // Rebeca
-     {show:"rebecarey",
-     namelg: "Rebeca Reyes Siqueiros",
-      namesm:"Rebeca Reyes",
-     img: "../../imgs/Equipo/rebe.png",
-     prof: "Estudiante de Ing. Mecatrónica",
-     description: "Ingeniera Mecatrónica graduada del Tecnológico de Monterrey, emprendedora social apasionada, entusiasta y sensible. Apasionada por el feminismo, la justicia, la tecnología y la política y participación ciudadana. - Quiero aportar a mi sociedad a través del amor, el liderazgo, el emprendimiento y la colaboración-"},
     //Laura
     {show:"lauraharo",
     namelg: "Laura Lorena Haro Ramírez",
