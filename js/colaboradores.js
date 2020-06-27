a511ede217ea434ddfbb077f244a81a1a8ed05bb
Valeria <valeria.toscano.95@hotmail.com>
2020-06-26 19:05:44 -0500

    marian add

--- js/colaboradores.js
+++ js/colaboradores.js
@@ -12,7 +12,14 @@ var colaboradores=[
     namesm: "Laura Haro",
     img:"../../imgs/Equipo/laura.png",
     prof:"Secretaria Nacional de Vinculación con la Sociedad",
-    description:"Licenciada en Derecho por la Universidad de Guadalajara, Maestra en Intervención Social en las Sociedades del Conocimiento por la Universidad de la Rioja en España. Actualmente trabaja como Secretaria Nacional de Vinculación con la Sociedad, en la Secretaría del Comité Ejecutivo Nacional del PRI, que busca a través de diferentes mecanismos la vinculación con organizaciones de la sociedad civil, para su fortalecimiento, impulso de agenda legislativa y de política pública con este sector y el PRI. Cuenta con amplia experiencia en política pública, ha participado en eventos nacionales e internacionales como el World Urban Forum 2014 en Medellín, Colombia y el Foro de Derechos Humanos y la Eliminación de Violencia contra las mujeres 2016 en Guadalajara Jalisco, ha sido ponente en diversos foros y congresos de nivel nacional. Socia fundadora de la Asociación Civil Movimiento Propuesta Ciudadana, y de la Organización Civil Colomos es de Todos."}]
+    description:"Licenciada en Derecho por la Universidad de Guadalajara, Maestra en Intervención Social en las Sociedades del Conocimiento por la Universidad de la Rioja en España. Actualmente trabaja como Secretaria Nacional de Vinculación con la Sociedad, en la Secretaría del Comité Ejecutivo Nacional del PRI, que busca a través de diferentes mecanismos la vinculación con organizaciones de la sociedad civil, para su fortalecimiento, impulso de agenda legislativa y de política pública con este sector y el PRI. Cuenta con amplia experiencia en política pública, ha participado en eventos nacionales e internacionales como el World Urban Forum 2014 en Medellín, Colombia y el Foro de Derechos Humanos y la Eliminación de Violencia contra las mujeres 2016 en Guadalajara Jalisco, ha sido ponente en diversos foros y congresos de nivel nacional. Socia fundadora de la Asociación Civil Movimiento Propuesta Ciudadana, y de la Organización Civil Colomos es de Todos."},
+    //Mariangela
+    {show:"mariantoscano",
+    namelg: "Mariángela Toscano Alcalá",
+    namesm: "Marian Toscano",
+    img:"../../imgs/Equipo/marian.png",
+    prof:"Equipo creativo",
+    description:"Estudiante del Tecnológico de Monterrey, con gusto por iniciativas sociales, con interés en el área de veterinaria y zootecnia, colaboradora en el área creativa y audiovisual de Vinner"}]
 
 module.exports=colaboradores;
     
